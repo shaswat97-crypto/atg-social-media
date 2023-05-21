d543e7fdb23cc5c30d9e78346ab9c14d6fd07ad8
shaswat97-crypto <shaswat97@gmail.com>
2023-05-22 00:54:20 +0530

    homepage styling

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import "./home.css";
 
 function Home() {
   const [postContent, setPostContent] = useState("");
@@ -23,6 +24,8 @@ function Home() {
       content: postContent,
     };
     const res = await axios.post("http://localhost:5000/api/v1/posts", obj);
+    setClicked((prev) => !prev);
+    setPostContent("");
     console.log(res.data);
   };
 
@@ -33,7 +36,7 @@ function Home() {
         case "Update":
           setCurrid(id);
           break;
-        case "Post":
+        case "Update post":
           let obj = {
             content: updateText,
           };
@@ -51,6 +54,8 @@ function Home() {
             `http://localhost:5000/api/v1/posts/${id}`
           );
           console.log(dres);
+          setCurrid(-1);
+          setUpdateText("");
           setClicked((prev) => !prev);
           break;
         case "Like":
@@ -67,47 +72,68 @@ function Home() {
           console.log(dlres);
           setClicked((prev) => !prev);
           break;
+        case "Cancel":
+          setCurrid(-1);
+          setUpdateText("");
+          setClicked((prev) => !prev);
+          break;
       }
     }
   };
   return (
-    <div>
-      <textarea
-        value={postContent}
-        onChange={(e) => setPostContent(e.target.value)}
-        className="postContent"
-        cols="30"
-        rows="10"
-      ></textarea>
-      <button onClick={handleAdd}>Add new post</button>
-      <div>
-        {posts && (
-          <>
-            {posts.map((post) => (
-              <div
-                key={post._id}
-                onClick={(e) => handleClick(e, post._id)}
-                className="singlepostcont"
-              >
-                {post._id === currid && (
-                  <div>
-                    <textarea
-                      value={updateText}
-                      onChange={(e) => setUpdateText(e.target.value)}
-                      cols="30"
-                      rows="10"
-                    ></textarea>
-                    <button>Post</button>
-                  </div>
-                )}
-                <div>{post.content}</div>
-                <button disabled={currid != -1}>Update</button>
-                <button>Delete</button>
-                <button>{post.likes ? "Dislike" : "Like"}</button>
-              </div>
-            ))}
-          </>
-        )}
+    <div className="homecont">
+      <div className="home">
+        <textarea
+          placeholder="What's on your mind..."
+          value={postContent}
+          onChange={(e) => setPostContent(e.target.value)}
+          className="postContent"
+          cols="30"
+          rows="10"
+        ></textarea>
+        <button className="addnew" onClick={handleAdd}>
+          Add new post
+        </button>
+        <div className="posts">
+          <h2>All posts</h2>
+          {posts && (
+            <>
+              {posts.map((post) => (
+                <div
+                  key={post._id}
+                  onClick={(e) => handleClick(e, post._id)}
+                  className="singlepostcont"
+                >
+                  {post._id === currid && (
+                    <div className="updatewindow">
+                      <textarea
+                        placeholder={post.content}
+                        value={updateText}
+                        onChange={(e) => setUpdateText(e.target.value)}
+                        cols="30"
+                        rows="10"
+                      ></textarea>
+                      <div className="bcont">
+                        <button className="updatepost">Update post</button>
+                        <button className="updatepost">Cancel</button>
+                      </div>
+                    </div>
+                  )}
+                  {post._id !== currid && (
+                    <div className="posttext">{post.content}</div>
+                  )}
+                  <button className="button-18" disabled={currid != -1}>
+                    Update
+                  </button>
+                  <button className="button-18">Delete</button>
+                  <button className="button-1">
+                    {post.likes ? "Dislike" : "Like"}
+                  </button>
+                </div>
+              ))}
+            </>
+          )}
+        </div>
       </div>
     </div>
   );
