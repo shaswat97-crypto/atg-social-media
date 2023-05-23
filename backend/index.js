1e99b271990d79befcefcce53c1552b55b92f6fc
shaswat97-crypto <shaswat97@gmail.com>
2023-05-23 21:04:00 +0530

    static config

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,14 @@
 import express from "express";
 import mongoose from "mongoose";
 // import cors from "cors";
-import Router from './routes.js'
-import * as dotenv from 'dotenv'
-dotenv.config()
+import Router from "./routes.js";
+import * as dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config();
 
 mongoose
   .connect(process.env.MONGO_URL)
@@ -16,8 +21,13 @@ mongoose
 
 const app = express();
 // app.use(cors());
-app.use(express.json());
-app.use('/api/v1', Router);
+app
+  .use(express.json())
+  .use(express.static(path.resolve(__dirname, process.env.PUBLIC_DIR)))
+  .use("/api/v1", Router)
+  .use("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "build", "index.html"));
+  });
 
 app.listen(process.env.PORT, () => {
   console.log(`Server started on port ${process.env.PORT}`);
